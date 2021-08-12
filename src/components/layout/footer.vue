ef8ec2c7bf8596a4fd75bc5a75626e6872cc2542
ramandamaulana <masramanda@gmail.com>
2021-08-13 01:32:11 +0700

    ADMOON

---
+++ src/components/layout/footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+
+    <footer class="sticky-footer bg-white">
+      <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+          <span>Copyright &copy; Your Website 2020</span>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="home">
+    <div id="main">
+      <div id="wrapper">
+        <Sidebar />
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+          <!-- Main Content -->
+          <div id="content">
+            <Navbar />
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+              <!-- Page Heading -->
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="table-responsive">
+                        <table class="table table-bordered ">
+                          <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">First</th>
+                              <th scope="col">Last</th>
+                              <th scope="col">Handle</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">2</th>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                              <td>@fat</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">3</th>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <nav aria-label="Page navigation example">
+                          <ul class="pagination justify-content-end">
+                            <li class="page-item disabled">
+                              <a class="page-link" href="#" tabindex="-1"
+                                >Previous</a
+                              >
+                            </li>
+                            <li class="page-item">
+                              <a class="page-link" href="#">1</a>
+                            </li>
+                            <li class="page-item">
+                              <a class="page-link" href="#">2</a>
+                            </li>
+                            <li class="page-item">
+                              <a class="page-link" href="#">3</a>
+                            </li>
+                            <li class="page-item">
+                              <a class="page-link" href="#">Next</a>
+                            </li>
+                          </ul>
+                        </nav>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.container-fluid -->
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from "../components/layout/navbar.vue";
+import Sidebar from "../components/layout/sidebar.vue";
+import Footer from "../components/layout/footer";
+export default {
+  components: {
+    Sidebar,
+    Navbar,
+    Footer,
+  },
+};
+</script>
