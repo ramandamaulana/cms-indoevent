f4e66117dd202194f9387dd157daf3fc86199ffd
ramandamaulana <masramanda@gmail.com>
2021-08-14 14:41:46 +0700

    Login

---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-lg-12 bg-gradient-primary">
+        <div class="container">
+          <!-- Outer Row -->
+          <div class="row justify-content-center ">
+            <div class="col-xl-5 col-lg-12 col-md-9">
+              <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                  <!-- Nested Row within Card Body -->
+                  <div class="row">
+                    <div class="col-lg-12">
+                      <div class="p-5">
+                        <div class="text-center">
+                          <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                        </div>
+                        <form class="user">
+                          <div class="form-group">
+                            <input
+                              type="email"
+                              class="form-control form-control-user"
+                              id="exampleInputEmail"
+                              aria-describedby="emailHelp"
+                              placeholder="Enter Email Address..."
+                            />
+                          </div>
+                          <div class="form-group">
+                            <input
+                              type="password"
+                              class="form-control form-control-user"
+                              id="exampleInputPassword"
+                              placeholder="Password"
+                            />
+                          </div>
+                          <a
+                            href="index.html"
+                            class="btn btn-primary btn-user btn-block"
+                          >
+                            Login
+                          </a>
+                        </form>
+                        <hr />
+                        <div class="text-center">
+                          <a class="small" href="forgot-password.html"
+                            >Forgot Password?</a
+                          >
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.bg-gradient-primary {
+  width: 100% !important;
+  height: 100vh !important;
+}
+</style>
