5b0be8046af9d5c9920ac3eccec3c76b83708071
ramandamaulana <masramanda@gmail.com>
2021-08-29 16:37:19 +0700

    User attendance

---
+++ src/service/userattendance.service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const user = JSON.parse(localStorage.getItem("user"));
+export default {
+    getAll() {
+        return axios({method: 'get',
+        url:"http://127.0.0.1:8000/api/admin/user-attendance",
+        headers: {
+            'Authorization': "Bearer " + user.data.access_token,
+            'X_USER_ID': user.data.id,
+            'Content-Type': "application/json",
+          }
+          })
+        .then((response) => {
+          return response.data;
+        })
+        .catch((error) => {
+          return error.response.data;
+        }); 
+      },
+    }
---
+++ src/views/userattendance.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="home">
+    <div id="main">
+      <div id="wrapper">
+        <Sidebar />
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+          <!-- Main Content -->
+          <div id="content">
+            <Navbar />
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+              <!-- Page Heading -->
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="row ">
+                        <div class="col-lg-6 text-left">
+                          <h6
+                            style="font-family: 'Poppins'; font-size: 14px; font-weight: bold;"
+                          ></h6>
+                        </div>
+                        <div class="col-lg-6 text-right mb-3">
+                          <a
+                            class="btn btn-primary text-left"
+                            @click.prevent="handleCreate"
+                            ><i class="fa fa-plus mr-3"></i> Tambah</a
+                          >
+                        </div>
+                      </div>
+                      <vue-good-table
+                        :columns="columns"
+                        :rows="rows"
+                        :line-numbers="true"
+                        :search-options="{
+                          enabled: true,
+                        }"
+                        :pagination-options="{
+                          enabled: true,
+                        }"
+                      >
+                      </vue-good-table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.container-fluid -->
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from "../components/layout/navbar.vue";
+import Sidebar from "../components/layout/sidebar.vue";
+import Footer from "../components/layout/footer";
+import Userattendance from "../service/userattendance.service";
+export default {
+  components: {
+    Sidebar,
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      columns: [
+        {
+          label: "Nama",
+          field: "name",
+        },
+        {
+          label: "Ticket",
+          field: "ticket",
+        },
+        {
+          label: "Schedule",
+          field: "schedule",
+        },
+        {
+          label: "Status",
+          field: "status",
+        },
+      ],
+      rows: [],
+    };
+  },
+  created() {
+    Userattendance.getAll()
+      .then((response) => {
+        this.rows = response.rows;
+        console.log("Data Di Temukan", response.rows);
+      })
+      .catch((error) => {
+        console.log("Eror Data Tidak Di Temukan", error.response);
+      });
+  },
+};
+</script>
